030d46f20c58aa7471a41e25dd0919f7c2897a24
Yuriy Artamonov <artamonov@haulmont.com>
2017-10-08 09:20:03 +0400

    Use custom Javadoc CSS

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,19 @@
+/**
+ * Second test in application.
+ */
+public class DemoTest {
+
+    /**
+     * Test the first empty method.
+     */
+    public void testNew() {
+
+    }
+
+    /**
+     * Test the second empty method.
+     */
+    public void testEmpty() {
+
+    }
+}
